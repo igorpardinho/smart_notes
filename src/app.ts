c4e304b3cbd2b0725f8e52961ebe256b50e336b4
Igor Pardinho <pardinhoigor@gmail.com>
2025-08-23 17:01:49 -0300

    adiciona as rotas no contexto do app

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
 import express from "express";
 import { errorHandler } from "./middleware/errorHandler";
-
-
+import flashCardRoutes from "./modules/flashcards/flashcard.routes"
 const app = express();
-
 
 app.use(express.json());
 
-app.use(errorHandler)
+app.use("/flashcards",flashCardRoutes)
+app.use(errorHandler);
 export default app;

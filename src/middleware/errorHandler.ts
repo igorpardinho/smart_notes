3d34f0e9df49f08d75243d80c35b772bfa3bbf5c
Igor Pardinho <pardinhoigor@gmail.com>
2025-08-23 16:28:08 -0300

    feat(middleware): cria o middleware global para tratar as exceções

---
+++ src/middleware/errorHandler.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from "express";
+import { NotFoundError } from "../errors/NotFoundError";
+
+export const errorHandler = (
+  err: any,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
+  if (err.errors) {
+    return res.status(400).json({
+      status: "error",
+      message: "validação falhou",
+      details: err.errors,
+    });
+  }
+
+  if (err instanceof NotFoundError) {
+    return res
+      .status(err.statusCode)
+      .json({ status: "error", message: err.message });
+  }
+
+  return res.status(500).json({ error: "Erro interno do servidor" });
+};

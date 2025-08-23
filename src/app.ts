3a72ec6ce6084dd5706c27cbde5f06f2090253d9
Igor Pardinho <pardinhoigor@gmail.com>
2025-08-23 16:28:52 -0300

    feat(app): adiciona o errorHandler no contexto do app

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { errorHandler } from "./middleware/errorHandler";
 
 
 const app = express();
@@ -6,5 +7,5 @@ const app = express();
 
 app.use(express.json());
 
-
+app.use(errorHandler)
 export default app;
